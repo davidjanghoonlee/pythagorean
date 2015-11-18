00d28a16bfa301f9d9f4c55ce3fec0f8746d0b8b
David Lee <davidlee@seoulchristian.org>
2015-11-18 17:24:04 +0900

    2015-11-18 5:24PM

--- pythagorean.py
+++ pythagorean.py
@@ -1,7 +1,16 @@
 # Pythagorean Theorem a^2 + b^2 = h^2 , (where 'c' is the hypotenuse)
 #a = side_1 / b = side_2 /  h = side_3
+print "Which side is unknown? "
 
-choice
+print ("1. Side 1")
+print ("2. Side 2")
+print ("3. hypotenuse")
+
+
+choice input(": ")
+
+if choice == 1;
+    print 
 
 
 a = int(input("Insert the length of side 1: "))
