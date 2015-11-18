e1997a06bd2d1c215daaa7216b6ed0fac0c031b2
David Lee <davidlee@seoulchristian.org>
2015-11-18 17:16:04 +0900

    pythagorean program 2015-11-18

---
+++ pythagorean.py
@@ -0,0 +1,12 @@
+# Pythagorean Theorem a^2 + b^2 = h^2 , (where 'c' is the hypotenuse)
+#a = side_1 / b = side_2 /  h = side_3
+
+choice
+
+
+a = int(input("Insert the length of side 1: "))
+b = int(input("Insert the length of side 2: "))
+h = int(input("Insert the length of the hypotenuse: "))
+
+def side_1 (b,h)
+    return
