102b4ee986518414ac68a87e47da763dcd38b399
David Lee <davidlee@seoulchristian.org>
2015-11-19 17:26:15 +0900

    2015-11-19 5:26 PM

--- pythagorean.py
+++ pythagorean.py
@@ -1,29 +1,43 @@
-# Pythagorean Theorem a^2 + b^2 = h^2 , (where 'c' is the hypotenuse)
-#a = side_1 / b = side_2 /  h = side_3
+# Pythagorean Theorem a^2 + b^2 = h^2 , (where 'h' is the hypotenuse)
+import sys,string
 
-#First of all, give instructions
+#First of all, print the question.
 print "Which side is unknown? "
 
-# what does the user want to know about?
+# Print choices for the question.
 print ("1. Side 1")
 print ("2. Side 2")
 print ("3. hypotenuse")
+print ("4. Quit")
 
-#Choice function
-choice = input("Type your Choice 1/2/3: ")
+#Choice function: Let the user type his / her choice
+choice = input("Type your Choice 1/2/3/4: ")
 
+#If the choice is 1, the program will do these things.
 if choice == 1:
+    #let the user type the two sides that he / she knows
     b = float(input("Insert the length of side 2: "))
     h = float(input("Insert the length of the hypotenuse: "))
+    #calculate and print the result
     print "The length of Side 1 is: " ,(h**2 - b**2)**0.5 ,"."
+
+#If the choice is not 1 but 2, the program will do these things
 elif choice == 2:
+    #let the user type the two sides that he / she knows
     a = float(input("Insert the length of side 1: "))
     h = float(input("Insert the length of the hypotenuse: "))
+    #calculate and print the result
     print "The length of Side 2 is: " ,(h**2 - a**2)**(1/2) ,"."
+
+#If the choice is neither 1 nor 2 , the program will 
 elif choice == 3:
+    #let the user type the two sides that he / she knows
     a = float(input("Insert the length of side 1: "))
     b = float(input("Insert the length of side 2: "))
+    #calculate and print the result
     print " The length of the hypotenuse is: " ,(a**2 + b**2)**(1/2), "."
+elif choice == 4:
+    sys.exit(0)
 else:
     print "Invalid Option. Please Try Again."
 
