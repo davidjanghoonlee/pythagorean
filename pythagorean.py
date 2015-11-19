a3d92c972cb14814043b51be3e688ed6ebe425e4
David Lee <davidlee@seoulchristian.org>
2015-11-19 16:19:53 +0900

    2015-11-19 4:19PM

--- pythagorean.py
+++ pythagorean.py
@@ -1,8 +1,6 @@
 # Pythagorean Theorem a^2 + b^2 = h^2 , (where 'c' is the hypotenuse)
 #a = side_1 / b = side_2 /  h = side_3
 
-def sqrt(x):
-    return x^0.5
 #First of all, give instructions
 print "Which side is unknown? "
 
@@ -12,26 +10,23 @@ print ("2. Side 2")
 print ("3. hypotenuse")
 
 #Choice function
-choice = input(": ")
+choice = input("Type your Choice 1/2/3: ")
 
 if choice == 1:
     b = float(input("Insert the length of side 2: "))
     h = float(input("Insert the length of the hypotenuse: "))
-    print "The length of Side 1 is: " ,(h^2 - b^2)^0.5 ,"."
+    print "The length of Side 1 is: " ,(h**2 - b**2)**0.5 ,"."
 elif choice == 2:
     a = float(input("Insert the length of side 1: "))
     h = float(input("Insert the length of the hypotenuse: "))
-    print "The length of Side 2 is: " ,(h^2 - a^2)^(1/2) ,"."
+    print "The length of Side 2 is: " ,(h**2 - a**2)**(1/2) ,"."
 elif choice == 3:
     a = float(input("Insert the length of side 1: "))
     b = float(input("Insert the length of side 2: "))
-    print " The length of the hypotenuse is: " ,(a^2 + b^2)^(1/2), "."
+    print " The length of the hypotenuse is: " ,(a**2 + b**2)**(1/2), "."
 else:
     print "Invalid Option. Please Try Again."
 
 #a = float(input("Insert the length of side 1: "))
 #b = float(input("Insert the length of side 2: "))
 #h = float(input("Insert the length of the hypotenuse: "))
-
-def side_1 (b,h):
-    return
